d46a5dc1b068a5bc5f5e649eed938ba63136e73e
Bjorn Van Acker <bjorn@sumocoders.be>
2023-05-24 19:25:59 +0200

    Add objects to level 1

---
+++ Assets/Tiled/objects.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="68" tileheight="96" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="22" height="96" source="../Images/Environment/props/antenna.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="34" source="../Images/Environment/props/banner-arrow.png"/>
+ </tile>
+ <tile id="2">
+  <image width="13" height="10" source="../Images/Environment/props/banner-floor.png"/>
+ </tile>
+ <tile id="3">
+  <image width="14" height="44" source="../Images/Environment/props/banner-open.png"/>
+ </tile>
+ <tile id="4">
+  <image width="13" height="35" source="../Images/Environment/props/banner-small.png"/>
+ </tile>
+ <tile id="5">
+  <image width="28" height="66" source="../Images/Environment/props/banners.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="30" source="../Images/Environment/props/control-box-1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="17" height="27" source="../Images/Environment/props/control-box-2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="62" height="30" source="../Images/Environment/props/control-box-3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="68" height="35" source="../Images/Environment/props/hotel-sign.png"/>
+ </tile>
+ <tile id="10">
+  <image width="35" height="92" source="../Images/Environment/props/banner-big/banner-big-preview.gif"/>
+ </tile>
+ <tile id="11">
+  <image width="27" height="78" source="../Images/Environment/props/banner-coke/banner-coke-preview.gif"/>
+ </tile>
+ <tile id="12">
+  <image width="19" height="48" source="../Images/Environment/props/banner-neon/banner-neon-preview.gif"/>
+ </tile>
+ <tile id="13">
+  <image width="13" height="47" source="../Images/Environment/props/banner-scroll/banner-scroll-preview.gif"/>
+ </tile>
+ <tile id="14">
+  <image width="19" height="76" source="../Images/Environment/props/banner-side/banner-side-preview.gif"/>
+ </tile>
+ <tile id="15">
+  <image width="36" height="13" source="../Images/Environment/props/banner-sushi/banner-sushi-preview.gif"/>
+ </tile>
+ <tile id="16">
+  <image width="21" height="18" source="../Images/Environment/props/monitorface/monitorface_preview.gif"/>
+ </tile>
+</tileset>
