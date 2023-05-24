014c5c0ff680e24d8dcc512cb5f92a7ddaf140e3
Bjorn Van Acker <info@bjornvanacker.be>
2023-05-24 20:54:30 +0200

    Add future ui assets

--- Assets/Tiled/objects.tsx
+++ Assets/Tiled/objects.tsx
@@ -1,55 +1,152 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="68" tileheight="96" tilecount="17" columns="0">
+<tileset version="1.10" tiledversion="1.10.0" name="objects" tilewidth="68" tileheight="96" tilecount="36" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="17">
   <image width="22" height="96" source="../Images/Environment/props/antenna.png"/>
  </tile>
- <tile id="1">
+ <tile id="18">
   <image width="16" height="34" source="../Images/Environment/props/banner-arrow.png"/>
  </tile>
- <tile id="2">
+ <tile id="19">
   <image width="13" height="10" source="../Images/Environment/props/banner-floor.png"/>
  </tile>
- <tile id="3">
+ <tile id="20">
   <image width="14" height="44" source="../Images/Environment/props/banner-open.png"/>
  </tile>
- <tile id="4">
-  <image width="13" height="35" source="../Images/Environment/props/banner-small.png"/>
- </tile>
- <tile id="5">
+ <tile id="21">
   <image width="28" height="66" source="../Images/Environment/props/banners.png"/>
  </tile>
- <tile id="6">
+ <tile id="22">
+  <image width="13" height="35" source="../Images/Environment/props/banner-small.png"/>
+ </tile>
+ <tile id="23">
   <image width="32" height="30" source="../Images/Environment/props/control-box-1.png"/>
  </tile>
- <tile id="7">
+ <tile id="24">
   <image width="17" height="27" source="../Images/Environment/props/control-box-2.png"/>
  </tile>
- <tile id="8">
+ <tile id="25">
   <image width="62" height="30" source="../Images/Environment/props/control-box-3.png"/>
  </tile>
- <tile id="9">
+ <tile id="26">
   <image width="68" height="35" source="../Images/Environment/props/hotel-sign.png"/>
  </tile>
- <tile id="10">
-  <image width="35" height="92" source="../Images/Environment/props/banner-big/banner-big-preview.gif"/>
+ <tile id="27">
+  <image width="35" height="92" source="../Images/Environment/props/banner-big/banner-big-1.png"/>
+  <animation>
+   <frame tileid="27" duration="250"/>
+   <frame tileid="28" duration="250"/>
+   <frame tileid="29" duration="250"/>
+   <frame tileid="30" duration="250"/>
+  </animation>
  </tile>
- <tile id="11">
-  <image width="27" height="78" source="../Images/Environment/props/banner-coke/banner-coke-preview.gif"/>
+ <tile id="28">
+  <image width="35" height="92" source="../Images/Environment/props/banner-big/banner-big-2.png"/>
  </tile>
- <tile id="12">
-  <image width="19" height="48" source="../Images/Environment/props/banner-neon/banner-neon-preview.gif"/>
+ <tile id="29">
+  <image width="35" height="92" source="../Images/Environment/props/banner-big/banner-big-3.png"/>
  </tile>
- <tile id="13">
-  <image width="13" height="47" source="../Images/Environment/props/banner-scroll/banner-scroll-preview.gif"/>
+ <tile id="30">
+  <image width="35" height="92" source="../Images/Environment/props/banner-big/banner-big-4.png"/>
  </tile>
- <tile id="14">
-  <image width="19" height="76" source="../Images/Environment/props/banner-side/banner-side-preview.gif"/>
+ <tile id="31">
+  <image width="27" height="78" source="../Images/Environment/props/banner-coke/banner-coke-1.png"/>
  </tile>
- <tile id="15">
-  <image width="36" height="13" source="../Images/Environment/props/banner-sushi/banner-sushi-preview.gif"/>
+ <tile id="32">
+  <image width="27" height="78" source="../Images/Environment/props/banner-coke/banner-coke-2.png"/>
  </tile>
- <tile id="16">
+ <tile id="33">
+  <image width="27" height="78" source="../Images/Environment/props/banner-coke/banner-coke-3.png"/>
+  <animation>
+   <frame tileid="31" duration="250"/>
+   <frame tileid="32" duration="250"/>
+   <frame tileid="33" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <image width="19" height="48" source="../Images/Environment/props/banner-neon/banner-neon-1.png"/>
+ </tile>
+ <tile id="35">
+  <image width="19" height="48" source="../Images/Environment/props/banner-neon/banner-neon-2.png"/>
+ </tile>
+ <tile id="36">
+  <image width="19" height="48" source="../Images/Environment/props/banner-neon/banner-neon-3.png"/>
+ </tile>
+ <tile id="37">
+  <image width="19" height="48" source="../Images/Environment/props/banner-neon/banner-neon-4.png"/>
+  <animation>
+   <frame tileid="34" duration="250"/>
+   <frame tileid="35" duration="250"/>
+   <frame tileid="36" duration="250"/>
+   <frame tileid="37" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <image width="13" height="47" source="../Images/Environment/props/banner-scroll/banner-scroll-1.png"/>
+  <animation>
+   <frame tileid="38" duration="150"/>
+   <frame tileid="39" duration="150"/>
+   <frame tileid="40" duration="150"/>
+   <frame tileid="41" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <image width="13" height="47" source="../Images/Environment/props/banner-scroll/banner-scroll-2.png"/>
+ </tile>
+ <tile id="40">
+  <image width="13" height="47" source="../Images/Environment/props/banner-scroll/banner-scroll-3.png"/>
+ </tile>
+ <tile id="41">
+  <image width="13" height="47" source="../Images/Environment/props/banner-scroll/banner-scroll-4.png"/>
+ </tile>
+ <tile id="42">
+  <image width="19" height="76" source="../Images/Environment/props/banner-side/banner-side-1.png"/>
+  <animation>
+   <frame tileid="42" duration="150"/>
+   <frame tileid="43" duration="150"/>
+   <frame tileid="44" duration="150"/>
+   <frame tileid="45" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="43">
+  <image width="19" height="76" source="../Images/Environment/props/banner-side/banner-side-2.png"/>
+ </tile>
+ <tile id="44">
+  <image width="19" height="76" source="../Images/Environment/props/banner-side/banner-side-3.png"/>
+ </tile>
+ <tile id="45">
+  <image width="19" height="76" source="../Images/Environment/props/banner-side/banner-side-4.png"/>
+ </tile>
+ <tile id="46">
+  <image width="36" height="13" source="../Images/Environment/props/banner-sushi/banner-sushi-1.png"/>
+  <animation>
+   <frame tileid="46" duration="150"/>
+   <frame tileid="47" duration="150"/>
+   <frame tileid="48" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <image width="36" height="13" source="../Images/Environment/props/banner-sushi/banner-sushi-2.png"/>
+ </tile>
+ <tile id="48">
+  <image width="36" height="13" source="../Images/Environment/props/banner-sushi/banner-sushi-3.png"/>
+ </tile>
+ <tile id="49">
   <image width="21" height="18" source="../Images/Environment/props/monitorface/monitorface_preview.gif"/>
+  <animation>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="50">
+  <image width="21" height="18" source="../Images/Environment/props/monitorface/monitor-face-1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="21" height="18" source="../Images/Environment/props/monitorface/monitor-face-2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="21" height="18" source="../Images/Environment/props/monitorface/monitor-face-3.png"/>
  </tile>
 </tileset>
