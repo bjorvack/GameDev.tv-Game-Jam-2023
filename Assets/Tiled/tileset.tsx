751f89af5510490c03c49637826bed2497759908
Bjorn Van Acker <bjorn@sumocoders.be>
2023-05-24 14:08:55 +0200

    Fix collisions on tiled levels

--- Assets/Tiled/tileset.tsx
+++ Assets/Tiled/tileset.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="tileset" tilewidth="16" tileheight="16" tilecount="384" columns="24">
  <image source="../Images/Environment/tileset.png" width="384" height="256"/>
+ <tile id="270">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9" y="0" width="7" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="271">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="272">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="273">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="7" height="14"/>
+  </objectgroup>
+ </tile>
 </tileset>
